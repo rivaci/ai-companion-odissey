574d7d71234ba51653daaf237f819d80bf6e3f30
Denis Rivas <denis_rivas@yahoo.fr>
2024-09-27 11:29:04 +0100

    Part 2 : streaming

---
+++ part2-memory/README.md
@@ -0,0 +1,10 @@
+# The Odyssey of Your AI Companion - Part 2: Observing and Strengthening Memory with Langsmith, LangGraph, Chainlit, and AWS Aurora
+A Step-by-Step Guide to Observing, Enhancing, and Empowering Your AI Companion
+
+<p align="center">
+  <img src="../images/ai-companion-odissey-part-2-memory.png">
+  <br>
+  <i>Created with MidJourney</i>
+</p>
+
+WORK IN PROGRESS
---
+++ part2-memory/chatbot.py
@@ -0,0 +1,59 @@
+import chainlit as cl
+from graph import flow  # Import the compiled LangGraph workflow
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages.ai import AIMessageChunk
+
+# Add authentication
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "chainlit"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+    
+# Define the starters
+@cl.set_starters
+def set_starters():
+    return [
+        cl.Starter(
+            label="Tell me something interesting !",
+            message="Give me an interesting fact about history or science.",
+            icon="/public/idea.svg"
+        ),
+        cl.Starter(
+            label="What is quantum computing?",
+            message="Explain what quantum computing is in simple terms.",
+            icon="/public/idea.svg"
+        ),
+        cl.Starter(
+            label="Explain photosynthesis",
+            message="Can you explain how photosynthesis works? Include the key steps in the process.",
+            icon="/public/idea.svg"
+        )
+    ]
+
+@cl.on_message
+async def main(message: cl.Message):
+
+    # Invoke the LangGraph flow to get the assistant's response
+    thread_id = cl.user_session.get("id")
+    config = {"configurable": {"thread_id": thread_id }}
+    input_message = HumanMessage(content=message.content)
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    async for event in flow.astream_events({"messages" : [input_message]}, config, version="v2"):
+        kind = event["event"]
+        if kind == "on_chat_model_stream":
+            chunk = event["data"]["chunk"]
+            if(isinstance(chunk, AIMessageChunk) and len(chunk.content) > 0 and chunk.content[-1].get("type", "") == "text"):
+                token = chunk.content[-1].get("text", "")
+                await msg.stream_token(token)
+
+    # Send a response back to the user
+    await msg.update()
